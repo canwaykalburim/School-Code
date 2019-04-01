9e28c3442c7e31fe1f85a68a300c45843bd1b6f1
Seodongyoung <36929558+canwaykalburim@users.noreply.github.com>
2019-04-01 19:37:36 +0900

    Add files via upload

---
+++ Decryption&Encryption/Decryption&Encryption.py
@@ -0,0 +1,24 @@
+print("암호화 할 평문을 입력하세요: ")
+
+secret = input()
+secret1 = ""
+
+print("변경할 만큼의 숫자를 입력하세요: ")
+
+k = int(input())
+
+for add in secret:
+    s = ord(add)
+    s = s + k
+    plus = chr(s)
+    secret1 += plus
+print("암호화 된 문자: " + secret1)
+
+secret1 = ""
+
+for add in secret1:
+    s = ord(add)
+    s = s - k
+    plus = chr(s)
+    secret += plus
+print("복호화 된 문자: " + secret)
